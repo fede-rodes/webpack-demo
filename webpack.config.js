08b0fb98ac388207dc140198addccb53273a74c5
fede-rodes <rodesf@gmail.com>
2017-05-08 21:36:30 +0200

    different builds for dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
-const webpack = require('webpack');
 // Notice that throughout the configuration we use Node's built-in path module
 // and prefix it with the __dirname global. This prevents file path issues
 // between operating systems and allows relative paths to work as expected. See
 // this section for more info on POSIX vs. Windows paths.
 const path = require('path');
+const webpack = require('webpack');
 
 const PRODUCTION = process.env.NODE_ENV === 'production';
 const DEVELOPMENT = process.env.NODE_ENV === 'development';
 
+const entry = PRODUCTION
+              ? ['./src/index.js']
+              : [
+                  './src/index.js',
+                  'webpack/hot/dev-server',
+                  'webpack-dev-server/client?http://localhost:8080',
+                ];
+const plugins = PRODUCTION ? [] : [new webpack.HotModuleReplacementPlugin()];
+
 // Default sintax for CommonJS module exports which can be understand by webpack? what about ES6?
 // See: https://webpack.js.org/configuration/
 const config = {
-  entry: [ // string | object | array
-    './src/index.js',
-    'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:8080',
-  ],
+  entry,  // string | object | array
   // Here the application starts executing and webpack starts bundling
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  plugins,
 
   output: {
 
